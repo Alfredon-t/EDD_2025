ae4c5536adc58d92ba007f39c9e20b4685be9cb1
Alfredon-t <lacv05@gmail.com>
2024-08-23 00:33:14 -0600

    Subida del archivo ConjuntoADT

---
+++ Tarea-2/ConjuntoADT.py
@@ -0,0 +1,40 @@
+class ConjuntoADT():
+    def __init__(self):
+        self.conjunto = set()
+
+    def longitud(self):
+        return len(self.conjunto)
+
+    def contiene(self, item):
+        return item in self.conjunto
+
+    def agregar(self, item):
+        if not self.contiene(item):
+            self.conjunto.add(item)
+
+    def eliminar(self, item):
+        if self.contiene(item):
+            self.conjunto.remove(item)
+
+    def equals(self, otroConjunto):
+        return self.conjunto == otroConjunto
+
+    def esSubConjunto(self, otroConjunto):
+        return self.conjunto.issubset(otroConjunto.conjunto)
+
+    def union(self, otroConjunto):
+        nuevoConjunto = ConjuntoADT()
+        nuevoConjunto.conjunto = self.conjunto.union(otroConjunto.conjunto)
+        return nuevoConjunto
+
+    def interseccion(self, otroConjunto):
+        nuevoConjunto = ConjuntoADT()
+        nuevoConjunto.conjunto = self.conjunto.intersection(
+            otroConjunto.conjunto)
+        return nuevoConjunto
+
+    def diferencia(self, otroConjunto):
+        nuevoConjunto = ConjuntoADT()
+        nuevoConjunto.conjunto = self.conjunto.difference(
+            otroConjunto.conjunto)
+        return nuevoConjunto
